563185748dfebc8a7e1117c8a43f92f34b2561d9
japarmar <55135328+japarmar@users.noreply.github.com>
2019-12-13 12:05:40 +0530

    Add files via upload

---
+++ userbot/plugins/design.py
@@ -0,0 +1,54 @@
+""".admin Plugin for @UniBorg"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from userbot.utils import admin_cmd
+
+
+@borg.on(admin_cmd("join"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "`━━━━━┓ \n┓┓┓┓┓┃\n┓┓┓┓┓┃　ヽ○ノ ⇦ Me When You Joined \n┓┓┓┓┓┃.     /　 \n┓┓┓┓┓┃ ノ) \n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃\n┓┓┓┓┓┃`"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+@borg.on(admin_cmd("pay"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "`█▀▀▀▀▀█░▀▀░░░█░░░░█▀▀▀▀▀█\n█░███░█░█▄░█▀▀░▄▄░█░███░█\n█░▀▀▀░█░▀█▀▀▄▀█▀▀░█░▀▀▀░█\n▀▀▀▀▀▀▀░▀▄▀▄▀▄█▄▀░▀▀▀▀▀▀▀\n█▀█▀▄▄▀░█▄░░░▀▀░▄█░▄▀█▀░▀\n░█▄▀░▄▀▀░░░▄▄▄█░▀▄▄▄▀▄▄▀▄\n░░▀█░▀▀▀▀▀▄█░▄░████ ██▀█▄\n▄▀█░░▄▀█▀█▀░█▄▀░▀█▄██▀░█▄\n░░▀▀▀░▀░█▄▀▀▄▄░▄█▀▀▀█░█▀▀\n█▀▀▀▀▀█░░██▀█░░▄█░▀░█▄░██\n█░███░█░▄▀█▀██▄▄▀▀█▀█▄░▄▄\n█░▀▀▀░█░█░░▀▀▀░█░▀▀▀▀▄█▀░\n▀▀▀▀▀▀▀░▀▀░░▀░▀░░░▀▀░▀▀▀▀`"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+@borg.on(admin_cmd("fk"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿\n🖕🏿🖕🏾🖕🏾🖕🏾🖕🏾🖕🏾🖕🏾🖕🏾🖕🏾🖕🏾🖕🏿\n🖕🏿🖕🏾🖕🏽🖕🏽🖕🏽🖕🏽🖕🏽🖕🏽🖕🏽🖕🏾🖕🏿\n🖕🏿🖕🏾🖕🏽🖕🏼🖕🏼🖕🏼🖕🏼🖕🏼🖕🏽🖕🏾🖕🏿\n🖕🏿🖕🏾🖕🏽🖕🏼🖕🏻🖕🏻🖕🏻🖕🏼🖕🏽🖕🏾🖕🏿\n🖕🏿🖕🏾🖕🏽🖕🏼🖕🏻🖕🖕🏻🖕🏼🖕🏽🖕🏾🖕🏿\n🖕🏿🖕🏾🖕🏽🖕🏼🖕🏻🖕🏻🖕🏻🖕🏼🖕🏽🖕🏾🖕🏿\n🖕🏿🖕🏾🖕🏽🖕🏼🖕🏼🖕🏼🖕🏼🖕🏼🖕🏽🖕🏾🖕🏿\n🖕🏿🖕🏾🖕🏽🖕🏽🖕🏽🖕🏽🖕🏽🖕🏽🖕🏽🖕🏽🖕🏿\n🖕🏿🖕🏾🖕🏾🖕🏾🖕🏾🖕🏾🖕🏾🖕🏾🖕🏾🖕🏾🖕🏿\n🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿🖕🏿"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
